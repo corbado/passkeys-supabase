2dc077d9caad42bae62057c9a26015f35371207f
Nicolai Stein <nicolai_stein@outlook.de>
2023-07-04 14:50:12 +0200

    cleanup

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,6 +1,5 @@
 import * as UserService from "../services/userService.js";
 import { SDK, Configuration } from "@corbado/node-sdk";
-import * as TodoService from "../services/todoService.js";
 
 const projectID = process.env.PROJECT_ID;
 const apiSecret = process.env.API_SECRET;
@@ -14,33 +13,6 @@ export const home = (req, res) => {
 
 export const login = (req, res) => {
   res.render("pages/login");
-};
-
-export const profileOld = async (req, res) => {
-  const { authenticated, email } = await corbado.session.getCurrentUser(req);
-
-  if (!authenticated) {
-    return res.redirect("/logout");
-  }
-
-  try {
-    const userId = await UserService.findIdByEmail(email);
-    const user = await UserService.findById(userId);
-    console.log("FindById result: ", user);
-    if (!user) {
-      res.redirect("/logout");
-    } else {
-      res.render("pages/profile", {
-        username: user.email,
-        userFullName: user.user_metadata.name,
-        supabaseID: user.id,
-        corbadoID: user.user_metadata.corbadoId,
-      });
-    }
-  } catch (err) {
-    console.log("Error: ", err);
-    res.status(500).send("Server Error");
-  }
 };
 
 export const logout = (req, res) => {
@@ -81,35 +53,5 @@ export const profile = async (req, res) => {
     console.log("500 error in auth redirect");
     console.error(err);
     res.status(500).send("Server Error");
-  }
-};
-
-export const authRedirect = async (req, res) => {
-  try {
-    const { email, name } = await corbado.session.getCurrentUser(req);
-    console.log("User email: ", email);
-    try {
-      console.log("Getting user by email: ", email);
-      const user = await UserService.findIdByEmail(email);
-      if (!user) {
-        // Create new user
-        console.log("Creating new user with email: ", email);
-        UserService.create(email, name).then((u) => {
-          console.log("User created: ", u);
-          res.redirect("/profile");
-        });
-      } else {
-        // User already exists
-        console.log("User found: ", user);
-        res.redirect("/profile");
-      }
-    } catch (err) {
-      console.log("500 error in auth redirect");
-      console.error(err);
-      res.status(500).send("Server Error");
-    }
-  } catch (err) {
-    console.log("401 error in auth redirect");
-    res.status(401).json({});
   }
 };
